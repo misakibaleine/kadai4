fc4da941fe58e6676c0bbf5a3bab83587849ad15
Misaki YAMAKAWA <z22089my@aiit.ac.jp>
2022-08-06 00:02:21 +0900

    if文の順番を考慮。確認項目追加

--- test_gcd.sh
+++ test_gcd.sh
@@ -5,19 +5,27 @@
 ans="/tmp/$$-ymisaki-ans"
 gcd="/tmp/$$-ymisaki-gcd"
 
+#入力が少ない場合
+#0の場合
+echo "エラー：自然数を2つ入力してください" > ${ans}
+./gcd.sh > ${gcd} && exit 1
+diff ${ans} ${gcd} || exit 1
+#1の場合
+echo "エラー：自然数を2つ入力してください" > ${ans}
+./gcd.sh 4 > ${gcd} && exit 1
+diff ${ans} ${gcd} || exit 1
+
 #0が入力されたとき
 echo "エラー：自然数を入力してください" > ${ans}
 ./gcd.sh 0 4 > ${gcd} && exit 1
 diff ${ans} ${gcd} || exit 1
 
 #マイナスが含まれるとき
+echo "エラー：数値にマイナスが含まれています" > ${ans}
+./gcd.sh -7 7 > ${gcd} && exit 1
+diff ${ans} ${gcd} || exit 1
 
 #文字列が含まれるとき
-
-#入力が少ない場合
-#echo "自然数を2つ入力してください" > {$ans}
-#./gcd.sh 4 2> {$gcd} && exit 1
-#diff ${ans} ${gcd} || exit 1
 
 #入力が多い場合
 
