d036a8a7f3a5687460ae2ac242f5cbb31daa4041
Misaki YAMAKAWA <z22089my@aiit.ac.jp>
2022-08-06 19:43:30 +0900

    判定文バグ修正

--- gcd.sh
+++ gcd.sh
@@ -21,28 +21,30 @@ then
 	echo "エラー：自然数を入力してください"
 	exit 1
 #数値にマイナスが含まれていないことを確認
-else [ $1 -lt 0 ] || [ $2 -lt 0 ]
+elif [ $1 -lt 0 ] || [ $2 -lt 0 ]
+then
 	echo "エラー：数値にマイナスが含まれています"
 	exit 1
-fi
+else
 	
 #最大公約数を求める
 
 t=$1
 
-if [ $1 -lt $2 ]
-then
-	t=$2
-fi
-
-while [ $t -ne 0 ]
-do
-	x=`expr $1 % $t`
-	y=`expr $2 % $t`
-	if [ $x -eq 0 ] && [ $y -eq 0 ]
-        then
-		echo "最大公約数は：$t"
-		break
+	if [ $1 -lt $2 ]
+	then
+		t=$2
 	fi
-	t=`expr $t - 1`
-done
+
+	while [ $t -ne 0 ]
+	do
+		x=`expr $1 % $t`
+		y=`expr $2 % $t`
+		if [ $x -eq 0 ] && [ $y -eq 0 ]
+        	then
+			echo "最大公約数は：$t"
+			break
+		fi
+		t=`expr $t - 1`
+	done
+fi
