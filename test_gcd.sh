a31be8a3b3577cbda8f518398f3b33aa1e0cdb78
Misaki YAMAKAWA <z22089my@aiit.ac.jp>
2022-08-06 15:23:01 +0900

    2つ以上の引数が入力されたときの条件を追加

--- test_gcd.sh
+++ test_gcd.sh
@@ -5,7 +5,7 @@
 ans="/tmp/$$-ymisaki-ans"
 gcd="/tmp/$$-ymisaki-gcd"
 
-#入力が少ない場合
+##入力が少ない場合
 #0の場合
 echo "エラー：自然数を2つ入力してください" > ${ans}
 ./gcd.sh > ${gcd} && exit 1
@@ -13,6 +13,16 @@ diff ${ans} ${gcd} || exit 1
 #1の場合
 echo "エラー：自然数を2つ入力してください" > ${ans}
 ./gcd.sh 4 > ${gcd} && exit 1
+diff ${ans} ${gcd} || exit 1
+
+##入力数が2よりも多い場合
+#3の場合
+echo "エラー：自然数を2つ入力してください" > ${ans}
+./gcd.sh 55 65 85  > ${gcd} && exit 1
+diff ${ans} ${gcd} || exit 1
+#5の場合
+echo "エラー：自然数を2つ入力してください" > ${ans}
+./gcd.sh 3 9 27 36 72 56 > ${gcd} && exit 1
 diff ${ans} ${gcd} || exit 1
 
 #0が入力されたとき
